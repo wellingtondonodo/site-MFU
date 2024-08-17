4c4926cfab660fee6b1c153f144d1be82d73a48b
wellingtondonodo s <bondedosmicos@gmail.com>
2024-08-17 05:14:31 +0000

    Página de Materiais Desktop

--- src/pages/Materiais.jsx
+++ src/pages/Materiais.jsx
@@ -2,11 +2,30 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { GlobalStyle } from "../styles/global"
 import {PaginaMateriais} from "../styles/Materiais.styles"
+import Arduino from "/Arduino.png"
 
 export default function Materiais() {
   return (
     <PaginaMateriais>
       <Header/>
+      <section>
+        <div id="texto">
+          <h2>Materiais utilizados</h2>
+          <div>
+            <p>Caixa Organizadora de Plástico</p>
+            <p>Arduino Uno</p>
+            <p>Jumpers variados (fios)</p>
+            <p>Monitor LCD   Protoboard 830 pontos</p>
+            <p>Sensor de temperatura NTC</p>
+            <p>Resistor</p>
+            <p>Álcool Etílico</p>
+            <p>Botão</p>
+            <p>Módulo Bluetooth HC-05</p>
+            <p>Silicone</p>
+          </div>
+        </div>
+        <img src={Arduino} alt="Arduino Uno" />
+      </section>
       <GlobalStyle/>
       <Footer/>
     </PaginaMateriais>
--- src/styles/Materiais.styles.js
+++ src/styles/Materiais.styles.js
@@ -1,5 +1,32 @@
 import styled from "styled-components"
 
 export const PaginaMateriais = styled.main`
-    
+    min-height: 100vh;
+    section{
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        padding-top: 9.5rem;
+        padding-bottom: 11.5rem;
+
+        p{
+            font-family: "Poppins";
+            font-size: 1.75rem;
+            font-weight: 300;
+        }
+
+        h2{
+            text-align: center;
+        }
+
+        img{
+            object-fit: contain;
+        }
+
+        #texto{
+            div{
+                margin-top: 3.5rem;
+            }
+        }
+    }
 `
