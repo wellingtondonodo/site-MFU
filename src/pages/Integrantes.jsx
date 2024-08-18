3f1abbbfccfcfcf615d94450b4db339bcd3cdfad
wellingtondonodo s <bondedosmicos@gmail.com>
2024-08-18 02:10:47 +0000

    Página Integrantes Desktop

--- src/pages/Integrantes.jsx
+++ src/pages/Integrantes.jsx
@@ -2,11 +2,43 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { GlobalStyle } from "../styles/global"
 import { PaginaIntegrantes } from "../styles/Integrantes.styles"
+import Guilherme from "/Guilherme.png"
+import Costa from "/Costa.png"
+import Alves from "/Alves.png"
+import Wellington from "/Wellington.png"
 
 export default function Integrantes() {
   return (
     <PaginaIntegrantes>
       <Header/>
+      <section>
+        <div id="texto">
+          <h2>Integrantes do projeto</h2>
+          <p>Somos um grupo de pesquisadores independentes que tem o intuito de melhorar a produção de frutas e vegetais</p>
+        </div>
+        <div id="integrantes">
+          <div>
+            <img src={Guilherme} alt="" />
+            <h3>Guilherme Pereira </h3>
+            <h4>GuilhermePerreira@gmail.com</h4>
+          </div>
+          <div>
+            <img src={Costa} alt="" />
+            <h3>João Pedro Costa</h3>
+            <h4>joao.pedro.scosta2006@gmail</h4>
+          </div>
+          <div>
+            <img src={Alves} alt="" />
+            <h3>João Pedro Alves</h3>
+            <h4>Fihdospop429@gmail.com</h4>
+          </div>
+          <div>
+            <img src={Wellington} alt="" />
+            <h3>Wellington Junior</h3>
+            <h4>wellingtonsoaresferreira.jr@gmail.com</h4>
+          </div>
+        </div>
+      </section>
       <GlobalStyle/>
       <Footer/>
     </PaginaIntegrantes>
--- src/styles/Integrantes.styles.js
+++ src/styles/Integrantes.styles.js
@@ -1,5 +1,53 @@
 import styled from "styled-components"
 
 export const PaginaIntegrantes = styled.main`
+    section{
+        padding-top: 7.625rem;
+        padding-bottom: 12.5rem;
+        min-height: calc(100vh - 13.875rem);
+    }
     
+    
+    #texto{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: justify;
+        margin-bottom: 4rem;
+        gap: 2.25rem;
+
+        p{
+            max-width: 36.5rem;
+            font-size: 1.75rem;
+            font-family: "Poppins";
+            font-weight: 300;
+        }
+    }
+
+    #integrantes{
+        display: flex;
+        justify-content: space-evenly;
+        font-family: "Poppins";
+
+        div{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        h3{
+            font-weight: 300;
+            font-size: 1.75rem;
+        }
+
+        h4{
+            font-size: 1rem;
+            font-weight: 300;
+        }
+
+        img{
+            border-radius: 15px;
+            max-width: 9.813rem;
+        }
+    }
 `
