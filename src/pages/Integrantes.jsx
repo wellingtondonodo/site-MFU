242f9fd6b3bbb6f74f30d68fa33f8ed00d871b5f
wellingtondonodo s <bondedosmicos@gmail.com>
2024-10-16 02:06:18 +0000

    Acabei

--- src/pages/Integrantes.jsx
+++ src/pages/Integrantes.jsx
@@ -6,41 +6,42 @@ import Guilherme from "/Guilherme.png"
 import Costa from "/Costa.png"
 import Alves from "/Alves.png"
 import Wellington from "/Wellington.png"
+import GuilhermeC from "/Guilherme.p.jpeg"
 
-export default function Integrantes() {
+export default function Integrantes({ darkMode }) {
   return (
-    <PaginaIntegrantes>
-      <Header/>
+    <PaginaIntegrantes className={darkMode ? 'dark' : ''}>
+      <Header darkMode={darkMode} />
       <section>
         <div id="texto">
           <h2>Integrantes do projeto</h2>
-          <p>Somos um grupo de pesquisadores independentes que tem o intuito de melhorar a produção de frutas e vegetais</p>
+          <p>Somos um grupo de pesquisadores independentes que tem o intuito de melhorar a produção de frutas e vegetais.</p>
         </div>
         <div id="integrantes">
           <div>
-            <img src={Guilherme} alt="" />
-            <h3>Guilherme Pereira </h3>
-            <h4>GuilhermePerreira@gmail.com</h4>
+          <img src={Wellington} alt="Wellington Junior" />
+            <h3>Wellington Junior</h3>
+            <h4>wellingtonsoaresferreira.jr@gmail.com</h4>
           </div>
           <div>
-            <img src={Costa} alt="" />
+            <img src={Costa} alt="João Pedro Costa" />
             <h3>João Pedro Costa</h3>
-            <h4>joao.pedro.scosta2006@gmail</h4>
+            <h4>joao.pedro.scosta2006@gmail.com</h4>
           </div>
           <div>
-            <img src={Alves} alt="" />
+            <img src={Alves} alt="João Pedro Alves" />
             <h3>João Pedro Alves</h3>
             <h4>Fihdospop429@gmail.com</h4>
           </div>
           <div>
-            <img src={Wellington} alt="" />
-            <h3>Wellington Junior</h3>
-            <h4>wellingtonsoaresferreira.jr@gmail.com</h4>
+          <img src={GuilhermeC} alt="Guilherme Pereira" />
+            <h3>Guilherme Pereira</h3>
+            <h4>GuilhermePerreira@gmail.com</h4>
           </div>
         </div>
       </section>
-      <GlobalStyle/>
-      <Footer/>
+      <GlobalStyle />
+      <Footer />
     </PaginaIntegrantes>
   )
 }
